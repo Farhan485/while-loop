838357a6848267ff61f2690f8af8bdb07cbb00e6
Sajjad5591 <sajjadirfan5591@gmail.com>
2025-07-04 23:20:50 -0700

    Add project files.

---
+++ while loop/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class WhileLoopExample
+{
+    static void Main()
+    {
+        int i = 1;
+
+        Console.WriteLine("Printing numbers from 1 to 5:");
+
+        while (i <= 5)
+        {
+            Console.WriteLine(i);
+            i++;
+        }
+
+        Console.WriteLine("Done!");
+        Console.ReadKey();
+    }
+}
